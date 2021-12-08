361d165fec7a7a78f8c623e201a18b317b52f140
sachin goel <goelsachin955@gmail.com>
2021-12-08 14:01:24 +0530

    done with validation error on 08-12-2021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
+import { CaptchaComponent } from './captcha/captcha.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationFormComponent,
+    CaptchaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/registration-form/registration-form.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-registration-form',
+  templateUrl: './registration-form.component.html',
+  styleUrls: ['./registration-form.component.css'],
+})
+export class RegistrationFormComponent implements OnInit {
+  dynamicFormArray: any;
+  captchaValueOne: any = Math.floor(Math.random() * 10);
+  captchaValueTwo: any = Math.floor(Math.random() * 10);
+  registrationForm: any;
+  totalValue_captcha: any;
+  constructor(private httpClient: HttpClient, private _fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.registrationForm = this._fb.group({
+   
+      inputCaptcha: [],
+    });
+
+    this.httpClient.get('/assets/db.json').subscribe((data) => {
+      this.dynamicFormArray = data;
+      console.log(this.dynamicFormArray);
+      this.formControl()
+    });
+  }
+
+  captcha_click() {
+    this.captchaValueOne = Math.floor(Math.random() * 10);
+    this.captchaValueTwo = Math.floor(Math.random() * 10);
+  }
+
+  submitHandler(registrationForm: any) {
+    this.totalValue_captcha = this.captchaValueOne + this.captchaValueTwo;
+    if (
+      this.totalValue_captcha ===
+      Number(registrationForm.controls.inputCaptcha.value)
+    ) {
+      alert('Login Successful');
+    } else if (registrationForm.controls.inputCaptcha.value === Number('')) {
+      alert('Enter Captcha value');
+    } else {
+      alert('Wrong Captcha Value');
+    }
+  }
+
+ formControl(){
+  this.dynamicFormArray.forEach((element:any)=>{
+    if(element.Required === true){
+      this.registrationForm.addControl(element.ID, new FormControl("",Validators.required))
+    }else {
+      this.registrationForm.addControl(element.ID, new FormControl(''))
+    }
+    console.log(this.registrationForm)
+  
+  })
+ }
+}
